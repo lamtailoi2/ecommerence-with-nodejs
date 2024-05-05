81083012502b3c90ea879bfea0afb0daa26b903f
lamtailoi2 <lamtailoi11141@gmail.com>
2024-05-05 23:27:20 +0700

    Add HomePage UI

---
+++ FE/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      textColor: {
+        "orange": "#FFB86C",
+        "dracula-comment": "#6272A4",
+        "dracula-yellow": "#F1FA8C"
+
+      },
+      backgroundColor: {
+        "dracula-yellow": "#f7f5be",
+        "dracula-theme": "#282A36"
+      }
+      ,
+      backgroundImage: {
+        "home": "url()"
+      }
+    },
+  },
+  plugins: [],
+}
